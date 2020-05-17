6f9874d336ddda3fbe46fe4516531036ab954b90
henriqueherrera <henriqueherrera9@gmail.com>
2020-05-17 16:25:59 -0300

    improvement: add task

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -5,122 +5,124 @@ import DateTimePicker from '@react-native-community/datetimepicker';
 import moment from 'moment';
 
 const initialState = {
-  desc: '',
-  date: new Date(),
-  showDatePicker: false
+	desc: '',
+	date: new Date(),
+	showDatePicker: false
 }
 
 export default class AddTask extends Component {
 
-  state = {
-    ...initialState
-  };
+	state = {
+		...initialState
+	};
 
-  save = () => {
-    const newTask = {
-      desc: this.state.desc,
-      date: this.state.date
-    };
+	save = () => {
+		const newTask = {
+			desc: this.state.desc,
+			date: this.state.date
+		};
 
-    this.props.onSave && this.props.onSave(newTask);
-    this.setState({ ...initialState })
-  }
-  getDatePicker = () => {
-    let datePicker = <DateTimePicker
-      value={this.state.date}
-      onChange={(_, date) => { this.setState({ date, showDatePicker: false }) }}
-      mode='date' />
-    if (this.state.date === undefined) {
-      this.state.date = initialState.date
-    }
-    const dateString = moment(this.state.date).format('dddd, D [de] MMMM [de] YYYY');
+		this.props.onSave && this.props.onSave(newTask);
 
-    if (Platform.OS === 'android') {
-      datePicker = (
-        <View>
-          <TouchableOpacity onPress={() => this.setState({ showDatePicker: true})}>
-            <Text style={styles.date}>
-              {dateString}
-            </Text>
-          </TouchableOpacity>
-          {this.state.showDatePicker && datePicker}
-        </View>
-      );
-    }
-    return datePicker
-  };
+		this.setState({ ...initialState })
+	}
 
-  render() {
-    console.log(this.props)
-    return (
-      <Modal transparent={true} visible={this.props.isVisible} onRequestClose={this.props.onCancel} animationType='slide'>
-        <TouchableWithoutFeedback onPress={this.props.onCancel}>
-          <View style={styles.background}></View>
-        </TouchableWithoutFeedback>
-        <View style={styles.container}>
-          <Text style={styles.header}>Nova Tarefa</Text>
-          <TextInput style={styles.input}
-            placeholder='Informe Descricao'
-            value={this.state.desc}
-            onChangeText={desc => this.setState({ desc })}
-          />
-          {this.getDatePicker()}
-          <View style={styles.buttons}>
-            <TouchableOpacity>
-              <Text style={styles.button} onPress={this.props.onCancel}>
-                Cancelar
-              </Text>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <Text style={styles.button}>
-                Salvar
-              </Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-        <TouchableWithoutFeedback onPress={this.props.onCancel}>
-          <View style={styles.background}></View>
-        </TouchableWithoutFeedback>
-      </Modal>
-    )
-  }
+	getDatePicker = () => {
+		let datePicker = <DateTimePicker
+			value={this.state.date}
+			onChange={(_, date) => { this.setState({ date, showDatePicker: false }) }}
+			mode='date' />
+		if (this.state.date === undefined) {
+			this.state.date = initialState.date
+		}
+		const dateString = moment(this.state.date).format('dddd, D [de] MMMM [de] YYYY');
+
+		if (Platform.OS === 'android') {
+			datePicker = (
+				<View>
+					<TouchableOpacity onPress={() => this.setState({ showDatePicker: true})}>
+						<Text style={styles.date}>
+							{dateString}
+						</Text>
+					</TouchableOpacity>
+					{this.state.showDatePicker && datePicker}
+				</View>
+			);
+		}
+		return datePicker
+	};
+
+	render() {
+		console.log(this.props)
+		return (
+			<Modal transparent={true} visible={this.props.isVisible} onRequestClose={this.props.onCancel} animationType='slide'>
+				<TouchableWithoutFeedback onPress={this.props.onCancel}>
+					<View style={styles.background}></View>
+				</TouchableWithoutFeedback>
+				<View style={styles.container}>
+					<Text style={styles.header}>Nova Tarefa</Text>
+					<TextInput style={styles.input}
+						placeholder='Informe Descricao'
+						value={this.state.desc}
+						onChangeText={desc => this.setState({ desc })}
+					/>
+					{this.getDatePicker()}
+					<View style={styles.buttons}>
+						<TouchableOpacity>
+							<Text style={styles.button} onPress={this.props.onCancel}>
+								Cancelar
+							</Text>
+						</TouchableOpacity>
+						<TouchableOpacity onPress={this.save}>
+							<Text style={styles.button}>
+								Salvar
+							</Text>
+						</TouchableOpacity>
+					</View>
+				</View>
+				<TouchableWithoutFeedback onPress={this.props.onCancel}>
+					<View style={styles.background}></View>
+				</TouchableWithoutFeedback>
+			</Modal>
+		)
+	}
 }
 
 const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    backgroundColor: 'rgba(0, 0, 0, 0.7)'
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#FFF'
-  },
-  header: {
-    backgroundColor: commonStyles.colors.today,
-    color: commonStyles.colors.secondary,
-    textAlign: 'center',
-    padding: 15,
-    fontSize: 20
-  },
-  input: {
-    height: 40,
-    margin: 15,
-    backgroundColor: '#FFF',
-    borderWidth: 1,
-    borderColor: '#E3E3E3',
-    borderRadius: 6
-  },
-  buttons: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end'
-  },
-  button: {
-    margin: 20,
-    marginRight: 30,
-    color: commonStyles.colors.today
-  },
-  date: {
-    fontSize: 20,
-    marginLeft: 15
-  }
+	background: {
+		flex: 1,
+		backgroundColor: 'rgba(0, 0, 0, 0.7)'
+	},
+	container: {
+		flex: 2,
+		backgroundColor: '#FFF'
+	},
+	header: {
+		backgroundColor: commonStyles.colors.today,
+		color: commonStyles.colors.secondary,
+		textAlign: 'center',
+		padding: 15,
+		fontSize: 20
+	},
+	input: {
+		height: 40,
+		margin: 15,
+		backgroundColor: '#FFF',
+		borderWidth: 1,
+		borderColor: '#E3E3E3',
+		borderRadius: 6
+	},
+	buttons: {
+		flexDirection: 'row',
+		justifyContent: 'flex-end'
+	},
+	button: {
+		margin: 20,
+		marginRight: 30,
+		color: commonStyles.colors.today
+	},
+	date: {
+		fontSize: 20,
+		marginLeft: 15
+	}
 })
--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
-import { View, Text, ImageBackground, StyleSheet, FlatList, TouchableOpacity, Platform } from 'react-native';
+import {
+	View,
+	Text,
+	ImageBackground,
+	StyleSheet,
+	FlatList,
+	TouchableOpacity,
+	Platform,
+	Alert
+} from 'react-native';
 import todayImage from '../../assets/imgs/today.jpg';
 import moment from 'moment';
 import 'moment/locale/pt-br';
@@ -10,124 +19,144 @@ import AddTask from '../components/AddTask';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class TaskList extends Component {
-  state = {
-    showDoneTasks: true,
-    showAddTasks: false,
-    visibleTasks: [],
-    tasks: [{
-      id: Math.random(),
-      desc: 'Comprar Livro react native',
-      estimateAt: new Date(),
-      doneAt: new Date()
-    }, {
-      id: Math.random(),
-      desc: 'ler Livro react native',
-      estimateAt: new Date(),
-      doneAt: null
-    }]
-  };
+	state = {
+		showDoneTasks: true,
+		showAddTasks: false,
+		visibleTasks: [],
+		tasks: [{
+			id: Math.random(),
+			desc: 'Comprar Livro react native',
+			estimateAt: new Date(),
+			doneAt: new Date()
+		}, {
+			id: Math.random(),
+			desc: 'ler Livro react native',
+			estimateAt: new Date(),
+			doneAt: null
+		}]
+	};
 
-  toggleTask = taskId => {
-    const tasks = [...this.state.tasks];
+	toggleTask = taskId => {
+		const tasks = [...this.state.tasks];
 
-    tasks.forEach(task => {
-      if (task.id == taskId) task.doneAt = task.doneAt ? null : new Date();
-    });
-    this.setState({ tasks }, this.filterTasks);
-  };
+		tasks.forEach(task => {
+			if (task.id == taskId) task.doneAt = task.doneAt ? null : new Date();
+		});
+		this.setState({ tasks }, this.filterTasks);
+	};
 
-  componentDidMount = () => {
-    this.filterTasks();
-  }
+	componentDidMount = () => {
+		this.filterTasks();
+	}
 
-  toggleFilter = () => {
-    this.setState({ showDoneTasks: !this.state.showDoneTasks }, this.filterTasks);
-  }
+	toggleFilter = () => {
+		this.setState({ showDoneTasks: !this.state.showDoneTasks }, this.filterTasks);
+	}
 
-  filterTasks = () => {
-    let visibleTasks = null;
+	filterTasks = () => {
+		let visibleTasks = null;
 
-    if (this.state.showDoneTasks) {
-      visibleTasks = [...this.state.tasks];
-    } else {
-      visibleTasks = this.state.tasks.filter(task => task.doneAt === null);
-    }
+		if (this.state.showDoneTasks) {
+			visibleTasks = [...this.state.tasks];
+		} else {
+			visibleTasks = this.state.tasks.filter(task => task.doneAt === null);
+		}
 
-    this.setState({ visibleTasks });
-  }
+		this.setState({ visibleTasks });
+	}
 
-  render() {
-    const today = moment().locale('pt-br').format('ddd, D [de] MMMM');
-    return (
-      <View style={styles.container}>
-        <AddTask isVisible={this.state.showAddTasks} onCancel={() => this.setState({ showAddTasks: false })} />
-        <ImageBackground source={todayImage} style={styles.background}>
-          <View style={styles.titleBar}>
-            <View style={styles.iconBar}>
-              <TouchableOpacity onPress={this.toggleFilter}>
-                <Icon name={this.state.showDoneTasks ? 'eye' : 'eye-slash'} size={20} color={commonStyles.colors.secondary} />
-              </TouchableOpacity>
-            </View>
-            <Text style={styles.title}>Hoje</Text>
-            <Text style={styles.subtitle}>{today}</Text>
-          </View>
-        </ImageBackground>
-        <View style={styles.taskList}>
-          <FlatList data={this.state.visibleTasks} keyExtractor={item => `${item.id}`} renderItem={({ item }) => <Task {...item} toggleTask={this.toggleTask} />} />
-        </View>
-        <TouchableOpacity style={styles.addButton}
-          activeOpacity={0.7}
-          onPress={() => this.setState({ showAddTasks: true })}
-        >
-          <Icon name='plus' size={20} color={commonStyles.colors.secondary} />
-        </TouchableOpacity>
-        <Text>developed by R.H</Text>
-      </View>
-    )
-  }
+	AddTask = newTask => {
+		if (!newTask.desc || !newTask.desc.trim()) {
+			Alert.alert('Dados Inválidos', 'Decrição não informada!');
+			return;
+		} 
+
+		const tasks = [...this.state.tasks];
+		tasks.push({
+			id: Math.random(),
+			desc: newTask.desc,
+			estimateAt: newTask.date,
+			doneAt: null
+		});
+
+		this.setState({  tasks, showAddTasks: false}, this.filterTasks);
+	}
+
+	render() {
+		const today = moment().locale('pt-br').format('ddd, D [de] MMMM');
+		return (
+			<View style={styles.container}>
+				<AddTask isVisible={this.state.showAddTasks}
+					onCancel={() => this.setState({ showAddTasks: false })}
+					onSave={this.AddTask}
+				/>
+				<ImageBackground source={todayImage} style={styles.background}>
+					<View style={styles.titleBar}>
+						<View style={styles.iconBar}>
+							<TouchableOpacity onPress={this.toggleFilter}>
+								<Icon name={this.state.showDoneTasks ? 'eye' : 'eye-slash'} size={20} color={commonStyles.colors.secondary} />
+							</TouchableOpacity>
+						</View>
+						<Text style={styles.title}>Hoje</Text>
+						<Text style={styles.subtitle}>{today}</Text>
+					</View>
+				</ImageBackground>
+				<View style={styles.taskList}>
+					<FlatList data={this.state.visibleTasks} keyExtractor={item => `${item.id}`} renderItem={({ item }) => <Task {...item} toggleTask={this.toggleTask} />} />
+				</View>
+				<TouchableOpacity style={styles.addButton}
+					activeOpacity={0.7}
+					onPress={() => this.setState({ showAddTasks: true })}
+				>
+					<Icon name='plus' size={20} color={commonStyles.colors.secondary} />
+				</TouchableOpacity>
+				<Text>developed by R.H</Text>
+			</View>
+		)
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  background: {
-    flex: 3
-  },
-  taskList: {
-    flex: 7
-  },
-  titleBar: {
-    flex: 1,
-    justifyContent: 'flex-end'
-  },
-  title: {
-    color: commonStyles.colors.secondary,
-    fontSize: 50,
-    marginLeft: 20,
-    marginBottom: 20
-  },
-  subtitle: {
-    color: commonStyles.colors.secondary,
-    fontSize: 20,
-    marginLeft: 20,
-    marginBottom: 30
-  },
-  iconBar: {
-    flexDirection: 'row',
-    marginHorizontal: 20,
-    justifyContent: 'flex-end',
-    marginTop: Platform.OS === 'ios' ? 40 : 10
-  },
-  addButton: {
-    position: 'absolute',
-    right: 30,
-    bottom: 30,
-    width: 50,
-    height: 50,
-    borderRadius: 25,
-    backgroundColor: commonStyles.colors.today,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+	container: {
+		flex: 1
+	},
+	background: {
+		flex: 3
+	},
+	taskList: {
+		flex: 7
+	},
+	titleBar: {
+		flex: 1,
+		justifyContent: 'flex-end'
+	},
+	title: {
+		color: commonStyles.colors.secondary,
+		fontSize: 50,
+		marginLeft: 20,
+		marginBottom: 20
+	},
+	subtitle: {
+		color: commonStyles.colors.secondary,
+		fontSize: 20,
+		marginLeft: 20,
+		marginBottom: 30
+	},
+	iconBar: {
+		flexDirection: 'row',
+		marginHorizontal: 20,
+		justifyContent: 'flex-end',
+		marginTop: Platform.OS === 'ios' ? 40 : 10
+	},
+	addButton: {
+		position: 'absolute',
+		right: 30,
+		bottom: 30,
+		width: 50,
+		height: 50,
+		borderRadius: 25,
+		backgroundColor: commonStyles.colors.today,
+		justifyContent: 'center',
+		alignItems: 'center'
+	}
 })
